ba8a55c0b8693480671ccf11a3d10acaf8b0fb40
zhangw <786014381@qq.com>
2022-02-21 10:44:27 +0800

    dist

---
+++ interview-bank-server/dist/question/mock/question.mock.d.ts
@@ -0,0 +1,22 @@
+export declare const questionClass: {
+    code: string;
+    name: string;
+    items: ({
+        code: string;
+        name: string;
+        items: {
+            code: string;
+            name: string;
+        }[];
+    } | {
+        code: string;
+        name: string;
+        items?: undefined;
+    })[];
+}[];
+export declare const question: {
+    question: string;
+    class: string;
+    level: number;
+    answer: string;
+}[];
---
+++ interview-bank-server/dist/question/question.controller.d.ts
@@ -0,0 +1,7 @@
+import { QuestionService } from './question.service';
+export declare class QuestionController {
+    private questionService;
+    constructor(questionService: QuestionService);
+    getClass(): any;
+    findClass(): any;
+}
