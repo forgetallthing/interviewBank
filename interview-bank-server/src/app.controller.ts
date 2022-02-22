a551459d7c32695a0c07e2e241566b306bc6f26d
zhangw <786014381@qq.com>
2022-02-22 17:09:19 +0800

    html server

---
+++ interview-bank-server/src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Get, Controller, Response } from '@nestjs/common';
+import { join } from 'path';
+
+@Controller()
+export class AppController {
+  @Get()
+  root(@Response() res) {
+    // res.sendFile(join(__dirname, '../public/', 'index.html'));
+    return res.render();
+  }
+}
--- interview-bank-server/src/main.ts
+++ interview-bank-server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { renderFile } from 'ejs';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('html', renderFile);
+  app.set('view engine', 'html');
+
   await app.listen(80);
 }
 bootstrap();
